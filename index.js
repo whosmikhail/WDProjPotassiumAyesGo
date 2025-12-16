0c5f94790f5e91d1752235b67f03f819c2d3897d
⋆˚࿔ 𝐚𝐬𝐭é𝐫𝐢 ! 𝜗𝜚˚⋆✮ <b2029lsaayes@pshs.edu.ph>
2025-12-16 11:30:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-let questionnum = 0
+let questionnum = 0;
 
-function CreateFlashcard() {
-
+function CreateFlashcard(e) {
     questionnum += 1;
 
     const question = document.getElementById("Question");
@@ -12,13 +11,21 @@ function CreateFlashcard() {
     const Qv = question.value;
     const Av = answer.value;
 
-    localStorage.setItem('question' + questionnum, Qv)
-    localStorage.setItem('answer' + questionnum, Av)
-    
-    // put question and answer in localstorage
+    localStorage.setItem('question' + questionnum, Qv);
+    localStorage.setItem('answer' + questionnum, Av);
+}
+
+window.addEventListener("load", () => {
+    const elements = document.querySelectorAll(".fade-up");
+    elements.forEach((el, i) => {
+        setTimeout(() => {
+            el.classList.add("show");
+        }, i * 150);
+    });
+});
+
+// put question and answer in localstorage
 
     // put the q&a from localstorage -> display
     //change id of the flashcard
     // create id of flashcard
-
-}
